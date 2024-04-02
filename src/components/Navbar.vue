ed12fc45ab7847496a117b242ad63f0e4aca2eee
Kamal Paudel <techitus07@gmail.com>
2024-04-02 15:50:16 +0545

    get learn

---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Create Blogs</li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+
+</script>
+
+ <style scoped>
+ nav {
+    background: black;
+    height: 50px;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 0 20px;
+    box-sizing: border-box;
+
+ }
+ ul {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    list-style: none;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+ }
+  li {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    list-style: none;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    font-size: 20px;
+    color: white;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  li:hover{
+opacity: 0.8;  }
+ </style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="container">
+
+  <div v-for="product in products" :key="product.id" class="card">
+    <img :src="product.image" alt="Product 1">
+    <div class="card-content">
+      <h3>{{product.name}}</h3>
+      <p>{{product.description}}</p> <br>
+      <p>{{product.material}}</p>
+      <p>{{product.createdAt}}</p>
+    </div>
+  </div>
+
+</div>
+  </template>
+
+<script>
+    import axios from 'axios'
+export default {
+    data(){
+        return {
+            products: []
+        }
+    },
+    methods: {
+      async  getProducts(){
+     const response =    await   axios.get("https://660bb1a1ccda4cbc75dd6dc3.mockapi.io/product")
+           this.products = response.data 
+
+        }
+
+    },
+    mounted(){
+        this.getProducts()
+    }
+
+}
+</script>
+
+<style src="./Home.css">
+
+</style>
